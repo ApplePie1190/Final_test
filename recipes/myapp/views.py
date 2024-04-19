cf6e19437e55ffeacacecc2c4a00b714a1bca6de
ApplePie <novikov1089@gmail.com>
2024-04-19 22:01:42 +0500

    main page

--- recipes/myapp/views.py
+++ recipes/myapp/views.py
@@ -1,4 +1,6 @@
+import random
 from django.shortcuts import render
+from .models import Recipe, Ingredient, Category
 
 import logging
 
@@ -6,5 +8,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    return render(request, "myapp/index.html")
+def home(request):
+    recipes = Recipe.objects.order_by("?")[:5]
+    return render(request, "myapp/home.html", {"recipes": recipes})
